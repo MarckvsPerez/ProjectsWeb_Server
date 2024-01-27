9c8bf380813fe4e768e5739e2e37107cf3984857
Marc Pérez Salat <marcantoniperez@gmail.com>
2024-01-27 13:53:24 +0100

    Feat: Created register endpoint

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,8 +1,29 @@
 import { type Request, type Response } from 'express';
+import bcrypt from 'bcrypt';
 import User from '../models/user';
 
-export function register(_req: Request, res: Response): void {
-	console.log(User);
+export async function register(req: Request, res: Response): Promise<void> {
+	const { firstname, lastname, email, password } = req.body;
 
-	res.status(200).send({ msg: 'OK' });
+	if (email === undefined) res.status(400).send({ msg: 'El email es obligatorio' });
+	if (password === undefined) res.status(400).send({ msg: 'La contraseña es obligatoria' });
+
+	const salt = bcrypt.genSaltSync(10);
+	const hashPassword = bcrypt.hashSync(password, salt);
+
+	const user = new User({
+		firstname,
+		lastname,
+		email: email.toLowerCase(),
+		password: hashPassword,
+		role: 'user',
+		active: false,
+	});
+
+	try {
+		const userStorage = await user.save();
+		res.status(200).send(userStorage);
+	} catch (error) {
+		res.status(400).send({ msg: 'El usuario ya existe' });
+	}
 }
